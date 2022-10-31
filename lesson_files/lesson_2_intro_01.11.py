89136ac4c6213ffc44be94aa3de3e871d4314399
Calvin-alis <alis.demiurg@gmail.com>
2022-10-31 16:09:02 +0200

    Add lesson_2 intro

---
+++ lesson_files/lesson_2_intro_01.11.py
@@ -0,0 +1,16 @@
+'''
+	Орієнтовно часу на вебінар: 2-2.30 години 
+	
+	Materials: 
+		https://www.w3schools.com/python/python_syntax.asp - from syntax to lists
+
+	Lesson 2 - 01.11.22
+	Topics:
+		1. Commands
+		2. Errors
+		3. Variables
+		4. Types  
+		5. Input
+		6. Numbers
+		7. Operation
+'''
--- lesson_files/start_python_29.10.py
+++ lesson_files/start_python_29.10.py
@@ -2,7 +2,7 @@
     Start lesson 29.09
     1. Install Python
     2. Create first code(hello_world.py)
-
 '''
 
-print('Hello World!')
+# print('Hello World!')
+
