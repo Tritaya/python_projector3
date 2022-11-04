e6844a21e76f0d5142e030dd0aa05804cb19a7be
Calvin-alis <alis.demiurg@gmail.com>
2022-11-04 14:03:32 +0200

    Add lesson_3 annotation

---
+++ lesson_files/lesson_3_bool_string_05.11.py
@@ -0,0 +1,24 @@
+'''
+    Python lesson - 05.11
+    Topics:
+        1. More operation with int/float( %, /, //)
+        2. Bool and operation with bool
+        3. String - operation with strings
+        4. format(f-string) string
+
+    Materials:
+    Bool:
+        1. https://realpython.com/python-operators-expressions/#logical-operators
+        2. https://www.w3schools.com/python/python_booleans.asp
+
+    String:
+    Materials
+        1. https://realpython.com/python-strings/
+        2. https://realpython.com/python-string-formatting/
+        3. https://realpython.com/python-string-split-concatenate-join/
+        4. https://realpython.com/python-f-strings/
+        5. https://www.w3schools.com/python/python_strings.asp
+        6. https://www.learnpython.org/en/String_Formatting
+        7. https://www.w3schools.com/python/python_ref_string.asp - methods string
+
+'''
