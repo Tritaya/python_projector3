d83943648c75709476261eaac1cb88c37cd638a2
Calvin-alis <alis.demiurg@gmail.com>
2022-11-07 17:26:37 +0200

    Add lesson_$

---
+++ lesson_files/lesson_4_control_flow_loops_08.11.py
@@ -0,0 +1,19 @@
+'''
+    Lesson 4. Control flow and loops. 08.11
+    Topics:
+        1. Logical operators
+        2. If/elif/else
+        3. Match-case *More that python version 10.
+        4. Loops(while/for)
+        5. Operation of break
+
+    Materials:
+        1. https://learnpython.com/blog/python-match-case-statement/ # Case statement
+        2. https://www.w3schools.com/python/python_conditions.asp
+        3. https://www.w3schools.com/python/python_while_loops.asp
+        4. https://www.w3schools.com/python/python_for_loops.asp
+        5. https://towardsdatascience.com/the-match-case-in-python-3-10-is-not-that-simple-f65b350bb025
+        6. https://realpython.com/python-conditional-statements/
+        7. https://python-course.eu/python-tutorial/conditional-statements.php
+
+'''
