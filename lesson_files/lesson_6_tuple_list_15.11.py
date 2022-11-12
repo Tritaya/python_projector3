ba7d1a6da5034160dc8a92ababa23b09dd932e09
Calvin-alis <alis.demiurg@gmail.com>
2022-11-12 17:40:10 +0200

    Add annotion for lesson_6

---
+++ lesson_files/lesson_6_tuple_list_15.11.py
@@ -0,0 +1,17 @@
+'''
+    Lesson 6. Tuple and operation. List and operation. 15.11
+    Topic:
+        1. Tuple
+        2. Operation with tuple
+        3. Tuple function
+        4. List/opearation with list
+        5. List function
+
+    Materials:
+        1. https://www.w3schools.com/python/python_arrays.asp
+        2. https://www.w3schools.com/python/python_lists.asp
+        3. https://realpython.com/python-scope-legb-rule/
+        4. https://realpython.com/python-lists-tuples/
+        5. https://www.learnpython.org - find lists/tuples
+
+'''
